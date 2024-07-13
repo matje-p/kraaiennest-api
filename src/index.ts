259f00a192c0a80410234cf77a6307e40bf0b0d0
matje-p <138682310+matje-p@users.noreply.github.com>
2024-07-13 22:17:49 +0200

    added localhost to middleware 2

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,10 @@ const app = express();
 
 // Update the CORS options to allow your frontend's domain
 const corsOptions = {
-  origin: 'https://kraaiennest.vercel.app', // Allow requests from this origin
+  origin: ['https://kraaiennest.vercel.app', 'http://localhost:3000'], // Allow requests from these origins
   optionsSuccessStatus: 200, // For legacy browser support
 };
+
 
 // Apply CORS middleware first
 app.use(cors(corsOptions));
