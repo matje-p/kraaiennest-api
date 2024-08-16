be898968443626b7257f5ea8b76d0632ed1db097
matje-p <138682310+matje-p@users.noreply.github.com>
2024-08-16 12:37:37 +0200

    overwritten build

--- dist/models/User.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const UserSchema = new mongoose_1.Schema({
-    name: { type: String, required: true },
-});
-const User = (0, mongoose_1.model)('User', UserSchema);
-exports.default = User;
