d941e47f9be266cbe4f1b08fc27e5c2673662b08
matje-p <138682310+matje-p@users.noreply.github.com>
2024-11-14 22:17:31 +0100

    added more logging

--- src/jwtMiddleware.ts
+++ src/jwtMiddleware.ts
@@ -7,6 +7,9 @@ dotenv.config({ path: envFile });
 
 const auth0Domain = process.env.AUTH0_DOMAIN
 const auth0Audience = process.env.AUTH0_AUDIENCE
+console.log('Auth0 Configuration:');
+console.log('Domain:', auth0Domain);
+console.log('Audience:', auth0Audience);
 
 // Create auth middleware
 const authMiddleware = auth({
